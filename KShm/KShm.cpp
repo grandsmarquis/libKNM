25f5371f152e47704999a1996c528c5a24d92afe
jean-philippe clipffel <jp.clipffel@gmail.com>
2014-05-09 16:02:28 +0200

    Updated KShm: - Adding input/output opertions - Adding seek support - Adding options for oppening SHM - Updated construtors

--- KShm/KShm.cpp
+++ KShm/KShm.cpp
@@ -23,7 +23,7 @@
 
 using namespace KNM;
 
-KShm::KShm():fd(-1){}
+KShm::KShm():fd(-1), oflags(O_CREAT|O_RDWR), omode(S_IRWXU|S_IRWXG|S_IRWXO){}
 
 /**
  * @param _ref Object to copy from
@@ -32,14 +32,16 @@ KShm::KShm(const KShm &_ref):fd(-1)
 {
 	if (&_ref != this)
 	{
-		name = _ref.name;
+		name   = _ref.name;
+		oflags = _ref.oflags;
+		omode  = _ref.omode;
 	}
 }
 
 /**
  * @param _name SHM name to create / open
  */
-KShm::KShm(const string &_name):fd(-1), name(_name){}
+KShm::KShm(const string &_name):fd(-1), name(_name), oflags(O_CREAT|O_RDWR), omode(S_IRWXU|S_IRWXG|S_IRWXO){}
 
 /**
  * @param _ref Object to copy from
@@ -49,8 +51,10 @@ KShm &KShm::operator=(const KShm &_ref)
 	if (&_ref != this)
 	{
 		close();
-		fd = -1;
-		name = _ref.name;
+		fd     = -1;
+		name   = _ref.name;
+		oflags = _ref.oflags;
+		omode  = _ref.omode;
 	}
 	return *this;
 }
@@ -60,12 +64,36 @@ KShm::~KShm()
 	close();
 }
 
+/**
+ * @param _name SHM name.
+ */
+void 	KShm::setName(const string &_name)
+{
+	name = _name;
+}
+
+/**
+ * @param _flags Open flags (see shm_open(2))
+ */
+void 	KShm::setOFlags(int _flags)
+{
+	oflags = _flags;
+}
+
+/**
+ * @param _mode Open mode (see shm_open(2))
+ */
+void 	KShm::setOMode(int _mode)
+{
+	omode = _mode;
+}
+
 void 	KShm::open()
 {
 	close();
 	if (name[0] != '/') {name.insert(0, "/");}
 	if (name.length() > NAME_MAX) {name.resize(NAME_MAX);}
-	fd = shm_open(name.c_str(), KSHM_DFLT_OFLAGS, KSHM_DFLT_MODE);
+	fd = shm_open(name.c_str(), oflags, omode);
 	if (fd < 0)
 		throw new KError(errno, "KShm::open()");
 }
@@ -96,4 +124,122 @@ void 	KShm::unlink()
 {
 	close();
 	shm_unlink(name.c_str());
+}
+
+/**
+ * @param _buffer Read from SHM to buffer.
+ * @param _len Read len bytes
+ */
+int 		KShm::read(void *_buffer, size_t _len)
+{
+	int 	ret;
+
+	ret = ::read(fd, _buffer, _len);
+	if (ret < 0 || (ret <= 0 && _len > 0) )
+		#if !defined KSHM_NO_EXCEPTIONS && !defined LIBKNM_NO_EXCEPTIONS
+			throw(new KError(errno, "KShm::read(void*)"));
+		#else
+			return ret;
+		#endif
+	return ret;
+}
+
+
+/**
+ * @param _buffer Read in this buffer
+ */
+int 		KShm::read(int &_buffer)
+{
+	int 	ret;
+	int 	buffer;
+
+	ret = ::read(fd, &buffer, sizeof(int));
+	if (ret != sizeof(int))
+		#if !defined KSHM_NO_EXCEPTIONS && !defined LIBKNM_NO_EXCEPTIONS
+			throw(new KError(errno, "KShm::read(int)"));
+		#else
+			return ret;
+		#endif
+	_buffer = buffer;
+	return ret;
+}
+
+/**
+ * @param _buffer Write this buffer content
+ * @param _len Write len bytes
+ */
+int 		KShm::write(const void *_buffer, size_t _len)
+{
+	int 	ret;
+
+	ret = ::write(fd, _buffer, _len);
+	if (ret < 0 || (ret <= 0 && _len > 0) )
+		#if !defined KSHM_NO_EXCEPTIONS && !defined LIBKNM_NO_EXCEPTIONS
+			throw(new KError(errno, "KShm::write(void*)"));
+		#else
+			return ret;
+		#endif
+	return ret;
+}
+
+/**
+ * @param _buffer Write this buffer
+ */
+int 	KShm::write(const int _buffer)
+{
+	int 	ret;
+
+	ret = ::write(fd, &_buffer, sizeof(int));
+	if (ret != sizeof(int))
+		#if !defined KSHM_NO_EXCEPTIONS && !defined LIBKNM_NO_EXCEPTIONS
+			throw(new KError(errno, "KShm::write(int)"));
+		#else
+			return ret;
+		#endif
+	return ret;
+}
+
+/**
+ * @param _buffer Write this buffer
+ */
+int 		KShm::write(const string &_buffer)
+{
+	int 	ret;
+
+	ret = ::write(fd, _buffer.c_str(), _buffer.size());
+	if (ret < 0 || (ret <= 0 && _buffer.size() > 0) )
+		#if !defined KSHM_NO_EXCEPTIONS && !defined LIBKNM_NO_EXCEPTIONS
+			throw(new KError(errno, "KShm::write(string)"));
+		#else
+			return ret;
+		#endif
+	return ret;
+}
+
+/**
+ * @param _offset lseek offset
+ * @param _whence lseek whence directive
+ */
+off_t 		KShm::seek(off_t _offset, int _whence)
+{
+	off_t 	ret;
+
+	ret = lseek(fd, _offset, _whence);
+	if (ret < 0)
+		#if !defined KSHM_NO_EXCEPTIONS && !defined LIBKNM_NO_EXCEPTIONS
+			throw(new KError(errno, "KShm::seek(enum offset)"));
+		#else
+			return ret;
+		#endif
+	return ret;
+}
+
+/**
+ * @param _offset BEGIN or END of SHM
+ */
+off_t 		KShm::seek(enum KShm::offset _offset)
+{
+	if (_offset == BEGIN)
+		return seek(0, SEEK_SET);
+	return seek(0, SEEK_END);
 }
--- KShm/KShm.hh
+++ KShm/KShm.hh
@@ -19,6 +19,7 @@
 * USA
 */
 
+#include <sys/types.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <errno.h>
@@ -29,7 +30,7 @@
 #include <KError.hh>
 
 #define 	KSHM_DFLT_OFLAGS	O_CREAT|O_RDWR
-#define 	KSHM_DFLT_MODE		S_IRWXU|S_IRWXG|S_IRWXO
+#define 	KSHM_DFLT_OMODE		S_IRWXU|S_IRWXG|S_IRWXO
 
 using namespace std;
 
@@ -37,26 +38,43 @@ namespace KNM
 {
 	/**
 	 * Manage shared memory.
+	 * Also provide SHM I/O methods.
 	 */
 	class 		KShm
 	{
 	public:
-		KShm();								/**< Default constructor. */
-		KShm(const string &);				/**< Specify a name during construction. */
-		KShm(const KShm&);					/**< Construct by copy. */
-		KShm &operator=(const KShm&);		/**< Assignment operator. */
-		~KShm();							/**< Destructor. */
+		enum offset {BEGIN, END};
 
-		void 	setName(const string &);	/**< Set SHM name. */
-		void 	open();						/**< Open the current SHM. */
-		void 	open(const string &);		/**< Open the SHM pointed by name. */
-		void 	close();					/**< Close the SHM. */
-		bool 	isOpen();					/**< Check if SHM is open. */
-		void 	unlink();					/**< Unlink the SHM. */
+	public:
+		KShm();									/**< Default constructor. */
+		KShm(const string &);					/**< Specify a name during construction. */
+		KShm(const KShm&);						/**< Construct by copy. */
+		KShm &operator=(const KShm&);			/**< Assignment operator. */
+		~KShm();								/**< Destructor. */
 
+		// SHM control
+		void 	setName(const string &);		/**< Set SHM name. */
+		void 	setOFlags(int);					/**< Set openning flags. */
+		void 	setOMode(int);					/**< Set openning mode. */
+		void 	open();							/**< Open the current SHM. */
+		void 	open(const string &);			/**< Open the SHM pointed by name. */
+		void 	close();						/**< Close the SHM. */
+		bool 	isOpen();						/**< Check if SHM is open. */
+		void 	unlink();						/**< Unlink the SHM. */
+
+		// I/O operations
+		int 	read(void*, size_t);			/**< Read some bytes from SHM.*/
+		int 	read(int&);						/**< Read an integer from SHM. */
+		int 	write(const void*, size_t);		/**< Write some bytes in SHM. */
+		int 	write(const int);				/**< Write an integer in SHM. */
+		int 	write(const string &);			/**< Write a string content in SHM. */
+		off_t 	seek(off_t, int);				/**< Direct access to lseek(). */
+		off_t 	seek(enum KShm::offset);		/**< Seek to the specified offset. */
 
 	private:
-		string 	name;						/**< SHM name. */
-		int 	fd;							/**< SHM file descriptor. */
+		int 	oflags;							/**< SHM open flags. */
+		int 	omode;							/**< SHM open mode. */
+		string 	name;							/**< SHM name. */
+		int 	fd;								/**< SHM file descriptor. */
 	};
 }
